10d0272ede119cf21d4cab53c241df2b04a0262d
Matej Medveď <matejmedved@s.zochova.sk>
2024-09-12 10:24:14 +0200

    uvodny commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+  return (
+  <Typography variant="h1" gutterBottom>
+    Bri´´ish matey chesse init
+  </Typography>
+  );
+}
