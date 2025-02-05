ba1e3df386299b39a781657d477c4d7994762e46
matej42 <matejmedved@s.zochova.sk>
2025-02-05 10:45:45 +0100

    auth_ide

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { getAllPosts } from "../actions/posts";
+import AuthKeeper from "../../components/AuthKeeper";
  
 export default function Home() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -24,25 +25,27 @@ export default function Home() {
   }, []);
  
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ textAlign: "center" }}
-    >
-      <Typography variant="h4">Recent Posts</Typography>
-      {posts.length === 0 ? (
-        <Typography>No posts available</Typography>
-      ) : (
-        posts.map((post) => (
-          <Box key={post.id} sx={{ maxWidth: "50%" }}>
-            <Typography variant="h6">{post.caption || "No caption"}</Typography>
-            <img src={post.imageUrl} alt={post.caption} style={{ width: "100%" }} />
-            <Typography variant="body1">Posted by: {post.user?.name || "Unknown User"}</Typography> {/* Show the user's name */}
-          </Box>
-        ))
-      )}
-    </Box>
+    <AuthKeeper>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ textAlign: "center" }}
+      >
+        <Typography variant="h4">Recent Posts</Typography>
+        {posts.length === 0 ? (
+          <Typography>No posts available</Typography>
+        ) : (
+          posts.map((post) => (
+            <Box key={post.id} sx={{ maxWidth: "50%" }}>
+              <Typography variant="h6">{post.caption || "No caption"}</Typography>
+              <img src={post.imageUrl} alt={post.caption} style={{ width: "100%" }} />
+              <Typography variant="body1">Posted by: {post.user?.name || "Unknown User"}</Typography> {/* Show the user's name */}
+            </Box>
+          ))
+        )}
+      </Box>
+    </AuthKeeper>
   );
 }
